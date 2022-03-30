c95bf81b37d86910871bf64138cca0d8f6965906
ferhi-fatah <101930870+ferhi-fatah@users.noreply.github.com>
2022-03-30 21:43:29 +0100

    Update currencies.js

--- currencies.js
+++ currencies.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const app = express()
-const currencies = require('./currencies.json')
 
-var chai = require('chai');
-var assert = chai.assert;    // Using Assert style
-var expect = chai.expect;    // Using Expect style
-var should = chai.should();  // Using Should style
+const currencies = require('./currencies.json')
 
 
 // ajouter un middleware :
@@ -22,69 +18,48 @@ app.get('/currencies/:rank', (req, res) => {
   res.status(200).json(currencie)
 })
 
-/*
-app.post('/currencies', (req, res) => {
-  currencies.push(req.body)
-  res.status(200).json(currencies)
-})
-
-*/
 const { body, validationResult } = require('express-validator');
 
-app.post(
-    '/currencies',
-
-    // symbol must be at least 2 chars long and max 4
-    body('symbol').isLength({ max: 4 }),
-    body('symbol').isLength({ min: 2 }),
-    (req, res) => {
-        // Finds the validation errors in this request and wraps them in an object with handy functions
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        currencie.create({
-            id: req.body.id,
-            symbol: req.body.symbol,
-            name: req.body.name,
-            supply: req.body.supply,
-            maxSupply: req.body.maxSupply,
-            marketCapUsd: req.body.marketCapUsd,
-            volumeUsd24Hr: req.body.volumeUsd24Hr,
-            priceUsd: req.body.priceUsd,
-            changePercent24Hr: req.body.changePercent24Hr,
-            vwap24Hr: req.body.vwap24Hr,
-        }).then(currencie => res.status(200).json(currencie));
-    },
-);
-
-
-app.put('/currencies/:rank', (req, res) => {
-    const rank = parseInt(req.params.rank)
-    let currencie = currencies.find(currencie => currencie.rank === rank)
-    currencie.id = req.body.id,
-      currencie.symbol = req.body.symbol,
-      currencie.name = req.body.name,
-      currencie.supply = req.body.supply,
-      currencie.maxSupply = req.body.maxSupply,
-      currencie.marketCapUsd = req.body.marketCapUsd,
-      currencie.volumeUsd24Hr = req.body.volumeUsd24Hr,
-      currencie.priceUsd = req.body.priceUsd,
-      currencie.changePercent24Hr = req.body.changePercent24Hr,
-      currencie.vwap24Hr = req.body.vwap24Hr,
-  
-  
-      res.status(200).json(currencie)
-  })
-  
-  app.delete('/currencies/:rank', (req, res) => {
-    const rank = parseInt(req.params.rank)
-    let currencie = currencies.find(currencie => currencie.rank === rank)
-    currencies.splice(currencies.indexOf(currencie), 1)
+app.post('/currencies',
+  body('symbol').isLength({ max: 4 }),
+  body('symbol').isLength({ min: 2 }),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    currencies.push(req.body)
     res.status(200).json(currencies)
   })
 
+
+app.put('/currencies/:rank', (req, res) => {
+  const rank = parseInt(req.params.rank)
+  let currencie = currencies.find(currencie => currencie.rank === rank)
+  currencie.id = req.body.id,
+    currencie.symbol = req.body.symbol,
+    currencie.name = req.body.name,
+    currencie.supply = req.body.supply,
+    currencie.maxSupply = req.body.maxSupply,
+    currencie.marketCapUsd = req.body.marketCapUsd,
+    currencie.volumeUsd24Hr = req.body.volumeUsd24Hr,
+    currencie.priceUsd = req.body.priceUsd,
+    currencie.changePercent24Hr = req.body.changePercent24Hr,
+    currencie.vwap24Hr = req.body.vwap24Hr,
+
+
+    res.status(200).json(currencie)
+})
+
+app.delete('/currencies/:rank', (req, res) => {
+  const rank = parseInt(req.params.rank)
+  let currencie = currencies.find(currencie => currencie.rank === rank)
+  currencies.splice(currencies.indexOf(currencie), 1)
+  res.status(200).json(currencies)
+})
+
+
+
 app.listen(8080, () => {
-    console.log("Serveur à l'écoute")
+  console.log("Serveur à l'écoute")
 })
