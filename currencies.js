b17529f1b84f696de4fa1a2131c2698f562af99e
ferhi-fatah <101930870+ferhi-fatah@users.noreply.github.com>
2022-03-30 21:28:03 +0100

    Update currencies.js

--- currencies.js
+++ currencies.js
@@ -2,28 +2,38 @@ const express = require('express')
 const app = express()
 const currencies = require('./currencies.json')
 
+var chai = require('chai');
+var assert = chai.assert;    // Using Assert style
+var expect = chai.expect;    // Using Expect style
+var should = chai.should();  // Using Should style
+
+
 // ajouter un middleware :
 app.use(express.json())
 
 
-
-app.get('/currencies', (req,res) => {
-    res.status(200).json(parkings)
+app.get('/currencies', (req, res) => {
+  res.status(200).json(currencies)
 })
 
-app.get('/currencies/:rank', (req,res) => {
-    const rank = parseInt(req.params.rank)
-    const currencie = currencies.find(currencie => currencie.rank === rank)
-    res.status(200).json(currencie)
+app.get('/currencies/:rank', (req, res) => {
+  const rank = parseInt(req.params.rank)
+  const currencie = currencies.find(currencie => currencie.rank === rank)
+  res.status(200).json(currencie)
 })
 
+/*
+app.post('/currencies', (req, res) => {
+  currencies.push(req.body)
+  res.status(200).json(currencies)
+})
 
-
+*/
 const { body, validationResult } = require('express-validator');
 
 app.post(
     '/currencies',
-    
+
     // symbol must be at least 2 chars long and max 4
     body('symbol').isLength({ max: 4 }),
     body('symbol').isLength({ min: 2 }),
@@ -48,36 +58,32 @@ app.post(
         }).then(currencie => res.status(200).json(currencie));
     },
 );
-/*
-app.post('/currencies', (req,res) => {
-    currencies.push(req.body)
-    res.status(200).json(currencies)
-})
-*/
-app.put('/currencies/:id', (req, res) => {
-    const id = parseInt(req.params.id)
+
+
+app.put('/currencies/:rank', (req, res) => {
+    const rank = parseInt(req.params.rank)
     let currencie = currencies.find(currencie => currencie.rank === rank)
     currencie.id = req.body.id,
-        currencie.symbol = req.body.symbol,
-        currencie.name = req.body.name,
-        currencie.supply = req.body.supply,
-        currencie.maxSupply = req.body.maxSupply,
-        currencie.marketCapUsd = req.body.marketCapUsd,
-        currencie.volumeUsd24Hr = req.body.volumeUsd24Hr,
-        currencie.priceUsd = req.body.priceUsd,
-        currencie.changePercent24Hr = req.body.changePercent24Hr,
-        currencie.vwap24Hr = req.body.vwap24Hr,
-
-
-        res.status(200).json(currencie)
-})
-
-app.delete('/currencies/:id', (req, res) => {
+      currencie.symbol = req.body.symbol,
+      currencie.name = req.body.name,
+      currencie.supply = req.body.supply,
+      currencie.maxSupply = req.body.maxSupply,
+      currencie.marketCapUsd = req.body.marketCapUsd,
+      currencie.volumeUsd24Hr = req.body.volumeUsd24Hr,
+      currencie.priceUsd = req.body.priceUsd,
+      currencie.changePercent24Hr = req.body.changePercent24Hr,
+      currencie.vwap24Hr = req.body.vwap24Hr,
+  
+  
+      res.status(200).json(currencie)
+  })
+  
+  app.delete('/currencies/:rank', (req, res) => {
     const rank = parseInt(req.params.rank)
     let currencie = currencies.find(currencie => currencie.rank === rank)
     currencies.splice(currencies.indexOf(currencie), 1)
     res.status(200).json(currencies)
-})
+  })
 
 app.listen(8080, () => {
     console.log("Serveur à l'écoute")
